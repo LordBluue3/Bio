1147dd87fd8eca0a73ecb073f9ed3dbf91277ffa
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-06-25 22:39:18 -0300

    Retirado as funções de animações

--- JS/script.js
+++ JS/script.js
@@ -1,73 +1,14 @@
-/*ANIMAÇÕES TAGS <a> E BOTÕES*/
-function mouseEnterMouse1() {
-    document.getElementById("portfolio").style = "color: black;";
-    document.querySelector("button").style = "background-color: #0c76ec;width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
-}
-function mouseLeaveMouse1() {
-    document.getElementById("portfolio").style = "color: white;";
-    document.querySelector("button").style = "background-image: linear-gradient(to left, #1d1fbf, #1950de, #0c76ec);width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
-}
-
-function mouseEnterMouse2() {
-    document.getElementById("github").style = "color: black;";
-    document.getElementById("button2").style = "background-color: #0c76ec;width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
-}
-function mouseLeaveMouse2() {
-    document.getElementById("github").style = "color: white;";
-    document.getElementById("button2").style = "background-image: linear-gradient(to left, #1d1fbf, #1950de, #0c76ec);width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
-}
-
-function mouseEnterMouse3() {
-    document.getElementById("linkedin").style = "color: black;";
-    document.getElementById("button3").style = "background-color: #0c76ec;width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
-}
-function mouseLeaveMouse3() {
-    document.getElementById("linkedin").style = "color: white;";
-    document.getElementById("button3").style = "background-image: linear-gradient(to left, #1d1fbf, #1950de, #0c76ec);width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
-}
-
-function mouseEnterMouse4() {
-    document.getElementById("discord").style = "color: black;";
-    document.getElementById("button4").style = "background-color: #0c76ec;width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
-}
-function mouseLeaveMouse4() {
-    document.getElementById("discord").style = "color: white;";
-    document.getElementById("button4").style = "background-image: linear-gradient(to left, #1d1fbf, #1950de, #0c76ec);width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
-}
-
-/*COR PADRÃO DOS BOTÕES*/
-document.querySelector("button").style = "background-image: linear-gradient(to left, #1d1fbf, #1950de, #0c76ec);width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
-document.getElementById("button2").style = "background-image: linear-gradient(to left, #1d1fbf, #1950de, #0c76ec);width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
-document.getElementById("button3").style = "background-image: linear-gradient(to left, #1d1fbf, #1950de, #0c76ec);width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
-document.getElementById("button4").style = "background-image: linear-gradient(to left, #1d1fbf, #1950de, #0c76ec);width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
-
-/*ANIMAÇÕES BOTÃO*/
-
-function clickButton() {
+function portfolio(){
     alert("Em Breve...");
-    document.querySelector("button").style = "background-color: #1d1fbf;width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
 }
-function unClickButton() {
-    document.querySelector("button").style = "background-image: linear-gradient(to left, #1d1fbf, #1950de, #0c76ec);width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
+function github(){
+    window.location.href = "https://github.com/LordBluue3";
 }
-
-function clickButton2() {
-    document.getElementById("button2").style = "background-color: #1d1fbf;width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
+function linkedin(){
+    window.location.href = "https://www.linkedin.com/in/mikael-oliveira-345a34209/";
 }
-function unClickButton2() {
-    document.getElementById("button2").style = "background-image: linear-gradient(to left, #1d1fbf, #1950de, #0c76ec);width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
+function discord(){
+    window.location.href = "https://discord.com/users/295038299133837313";
 }
 
-function clickButton3() {
-    document.getElementById("button3").style = "background-color: #1d1fbf;width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
-}
-function unClickButton3() {
-    document.getElementById("button3").style = "background-image: linear-gradient(to left, #1d1fbf, #1950de, #0c76ec);width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
-}
 
-function clickButton4() {
-    document.getElementById("button4").style = "background-color: #1d1fbf;width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
-}
-function unClickButton4() {
-    document.getElementById("button4").style = "background-image: linear-gradient(to left, #1d1fbf, #1950de, #0c76ec);width: 80%;padding: 15px 32px;color: white;font-weight: bold;border: none;display: inline-block;font-size: 16px;border-radius: 10px;cursor: pointer;margin: 2%;";
-}
