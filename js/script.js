8226e374402b76c5e06df145feaf332f8ab28a74
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-10-02 16:25:13 -0300

    Adicionando script

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function portfolio(){
+    alert("Em Breve...");
+}
+function github(){
+    window.location.href = "https://github.com/LordBluue3";
+}
+function linkedin(){
+    window.location.href = "https://www.linkedin.com/in/mikael-oliveira-345a34209/";
+}
+function discord(){
+    window.location.href = "https://discord.com/users/295038299133837313";
+}
